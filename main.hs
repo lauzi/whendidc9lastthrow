b72d94a196fd2fff7c69b2a53da3ec2731e4b5d6
LauZi <st61112@gmail.com>
2015-08-02 01:09:48 +0900

    Basic HTML

--- main.hs
+++ main.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import Dota
@@ -6,7 +8,7 @@ import DotaParser
 import Control.Lens
 import Control.Monad
 
-import Data.List (sortOn)
+import Data.List (sortOn, intersperse)
 
 import qualified Data.HashSet as S
 
@@ -16,6 +18,9 @@ import System.Directory (doesFileExist)
 
 import Text.Printf
 
+import Text.Blaze.Html5 as H
+import Text.Blaze.Html5.Attributes as A
+import Text.Blaze.Html.Renderer.String (renderHtml)
 
 isThrow :: Match -> Bool
 isThrow = (< -15000) . minimum . (^. winnerAdvantage)
@@ -40,15 +45,32 @@ readCache = do
 writeCache :: [Match] -> IO ()
 writeCache = writeFile cacheFilename . show
 
+getMatches :: IO [Match]
+getMatches = do
+  lostMatchIds <- getLostMatchIds c9TeamId
+  cachedMatches <- readCache
+
+  let cachedSet = S.fromList $ _matchId <$> cachedMatches
+      cached = (`S.member` cachedSet)
+      notCachedIds = filter (not . cached) lostMatchIds
+
+  lostMatchData <- forM notCachedIds getMatchData
+  let matches = lostMatchData ++ cachedMatches
+
+  unless (null notCachedIds) $ writeCache matches
+
+  return $ sortOn (^. matchTime) matches
+
 
 main :: IO ()
-main = mainCached
+main = genHtml
 
-mainCached :: IO ()
-mainCached = do
+
+printMatches :: IO ()
+printMatches = do
   matches <- getMatches
   forM_ matches $ \md -> printMatchInfo md >> putStrLn (replicate 40 '-')
-  printThrow matches
+  printLastThrow matches
 
 printMatchInfo :: Match -> IO ()
 printMatchInfo md = do
@@ -65,42 +87,50 @@ showThings :: String -> String -> Int -> String
 showThings sing _ 1 = "1 " ++ sing
 showThings _ prul n = printf "%d %s" n prul
 
-printThrow :: [Match] -> IO ()
-printThrow matches =
+printLastThrow :: [Match] -> IO ()
+printLastThrow matches =
   case filter isThrow matches ^? _last of
-    Nothing -> printf "This team acutally never threw!!!"
-    Just lastThrow -> reallyPrintThrow lastThrow
+    Nothing -> printf "They acutally never threw!!!"
+    Just lastThrow -> reallyPrintLastThrow lastThrow
 
-reallyPrintThrow :: Match -> IO ()
-reallyPrintThrow lastThrow = do
+reallyPrintLastThrow :: Match -> IO ()
+reallyPrintLastThrow lastThrow = do
   now <- getCurrentTime
   timezone <- getCurrentTimeZone
-
-  printf "%s's last throw was " $ lastThrow ^. matchTeams . loserDotabuff . teamName
 
   let throwTime = lastThrow ^. matchTime . to (localTimeToUTC timezone)
       diffHours = fromEnum $ diffUTCTime now throwTime / (3600 * 1e12) :: Int
       (days, hours) = diffHours `divMod` 24
 
+  printf "%s's last throw was " $ lastThrow ^. matchTeams . loserDotabuff . teamName
   printf "%s and %s ago " (showThings "day" "days" days) (showThings "hour" "hours" hours)
-
   printf "against %s.\n" $ lastThrow ^. matchTeams . winnerDotabuff . teamName
 
   printf "They threw away a %d gold lead.\n" $ lastThrow ^. winnerAdvantage . to minimum . to negate
 
 
-getMatches :: IO [Match]
-getMatches = do
-  lostMatchIds <- getLostMatchIds c9TeamId
-  cachedMatches <- readCache
+genHtml :: IO ()
+genHtml = do
+  matches <- getMatches
+  writeFile "c9.html" . renderHtml . htmlMatches $ matches
 
-  let cachedSet = S.fromList $ _matchId <$> cachedMatches
-      cached = (`S.member` cachedSet)
-      notCachedIds = filter (not . cached) lostMatchIds
+htmlMatches :: [Match] -> Html
+htmlMatches matches = docTypeHtml $ do
+  H.head $ H.title "When did Cloud 9 last throw"
+  body $ do
+    h1 "When did Cloud 9 last throw"
+    ol $ forM_ (reverse matches) (li . p . htmlMatch)
 
-  lostMatchData <- forM notCachedIds getMatchData
-  let matches = lostMatchData ++ cachedMatches
+htmlMatch :: Match -> Html
+htmlMatch md = sequence_ . intersperse br $ toHtml <$> content
+    where match = md ^. matchId
+          teams = md ^. matchTeams
+          gs = md ^. winnerAdvantage
 
-  unless (null notCachedIds) $ writeCache matches
-
-  return $ sortOn (^. matchTime) matches
+          content = [
+              printf "Match %s: " match
+            , printf "Time: %s" $ md ^. matchTime . to show
+            , printf "Winner: %s (%s)" (teams ^. winnerDotabuff . teamName) (teams ^. winner . to show)
+            , printf "Winner advantage: [%6d, %6d]" (minimum gs) (maximum gs)
+            , printf "It was %sa throw." $ if isThrow md then "" else "not " :: String
+            ] :: [String]
